946cf282614a34b352e27e6902a7d40ec2abd961
alex <mendelee23@bk.ru>
2022-11-19 18:00:59 +0300

    переделанно

--- app/src/main/java/com/example/aston5/MainActivity.kt
+++ app/src/main/java/com/example/aston5/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.aston5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val contactsList = ContactsList()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, contactsList)
     }
+
 }
